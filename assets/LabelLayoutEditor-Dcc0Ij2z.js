f73011c45ae1bab080fa1602e041e6d16f19c707
ruirui-1-cmd <2623028706@qq.com>
2026-08-29 11:13:11 +0800

    deploy: isolate label print sizing

--- assets/LabelLayoutEditor-Dcc0Ij2z.js
+++ assets/LabelLayoutEditor-Dcc0Ij2z.js
@@ -1,4 +1,4 @@
-import{c as Yr,r as Xc,a as xh,g as qc,b as Ut,R as Kt,j as tt,S as da,I as Dl,B as Gl,d as Vc,e as Qc}from"./index-DuSvhZ7-.js";/**
+import{c as Yr,r as Xc,a as xh,g as qc,b as Ut,R as Kt,j as tt,S as da,I as Dl,B as Gl,d as Vc,e as Qc}from"./index-C9x9j1jh.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
